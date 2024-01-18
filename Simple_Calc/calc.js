3015079cca5db91a7b3c80f8ea67e4dcd9d8a932
datta panchal <panchaldatta1508@gmail.com>
2024-01-17 20:22:33 -0800

    all files are added

---
+++ Simple_Calc/calc.js
@@ -0,0 +1,6 @@
+let firstNum = document.getElementById('first-num')
+let secNum = document.getElementById('sec-num')
+let sumEl = document.getElementById('sum-el')
+const multi = () =>{
+    sumEl.textContent += firstNum.textContent+secNum.textContent
+}
